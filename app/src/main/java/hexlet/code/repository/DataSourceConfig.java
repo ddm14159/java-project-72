eece031a614c7eaa4fa4ee8c2bdadad9436cdf4c
Ddm14159 <ddm14159@gmail.com>
2024-09-12 15:57:20 +0300

    db fix

--- app/src/main/java/hexlet/code/repository/DataSourceConfig.java
+++ app/src/main/java/hexlet/code/repository/DataSourceConfig.java
@@ -1,17 +1,13 @@
 package hexlet.code.repository;
 
 import com.zaxxer.hikari.HikariConfig;
-import lombok.extern.slf4j.Slf4j;
 
 import javax.sql.DataSource;
 
-@Slf4j
 public class DataSourceConfig {
 
     public static DataSource getDataSource() {
-        String jdbcUrl = System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1");
-        log.info("test2");
-        log.info(jdbcUrl);
+        String jdbcUrl = System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project");
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(jdbcUrl);
 
